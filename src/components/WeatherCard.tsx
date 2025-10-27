77de1a6ac33ca1c697fa46f4e5e83481849b51b5
shubhekshasharma <subu.sharma53@gmail.com>
2025-10-26 22:38:25 -0400

    Change styling of basic text data on the homepage by creating a new component

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -1,4 +1,5 @@
 import { CurrentWeather } from "@/types/weather";
+import { CurrentWeatherCard } from "./CurrentWeatherCard";
 
 /**
  * Basic weather card - no styling, just data display
@@ -13,12 +14,7 @@ interface WeatherCardProps {
 export function WeatherCard({ city, weather }: WeatherCardProps) {
   return (
     <div>
-      <h2>{city}</h2>
-      <p>{weather.temperature}°F</p>
-      <p>{weather.condition.description}</p>
-      <p>Feels like: {weather.feelsLike}°F</p>
-      <p>Humidity: {weather.humidity}%</p>
-      <p>Wind: {weather.windSpeed} mph</p>
+      <CurrentWeatherCard current={weather} city={city} />
     </div>
   );
 }
